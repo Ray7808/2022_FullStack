ddd9a5517f0516a51d95025ee6719ba9e16309db
Ray7808 <yuwang7808@gmail.com>
2022-05-26 10:28:36 +0800

    5/26_StartSecondPage

--- FrontEnd/Project3/app.js
+++ FrontEnd/Project3/app.js
@@ -3,3 +3,10 @@ const text = document.querySelectorAll(".thePaths");
 for (let i = 0; i < text.length; i++) {
   console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
 }
+
+const lastWord = document.querySelector("#first_16");
+const animation = document.querySelector("div.animation");
+
+lastWord.addEventListener("animationend", () => {
+  animation.style = "transition:all 1s;opacity:0;pointer-events:none;";
+});
